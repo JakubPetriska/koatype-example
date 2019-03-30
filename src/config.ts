ab51d3bbe6060c9aee964722a3f6d9f0335e0c65
Jakub Petriska <jakubpetriska@gmail.com>
2019-03-30 15:06:59 +0100

    Add missing types

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const PORT = process.env.PORT || 3000;
-export const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/koatype-example';
+export const PORT: string | number = process.env.PORT || 3000;
+export const MONGO_URL: string = process.env.MONGO_URL || 'mongodb://localhost:27017/koatype-example';
